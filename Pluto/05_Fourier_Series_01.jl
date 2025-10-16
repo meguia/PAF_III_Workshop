c061a2337e9c844e4202ef7cfe06cf9e535eb236
Manuel Eguia <eguiamc@gmail.com>
2025-10-16 12:26:11 +0200

    Update 05_Fourier_Series_01.jl

--- Pluto/05_Fourier_Series_01.jl
+++ Pluto/05_Fourier_Series_01.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.18
+# v0.20.19
 
 using Markdown
 using InteractiveUtils
@@ -30,13 +30,16 @@ include("../iii_utils.jl");
 # ╔═╡ 83f8450d-3225-4f37-ba5d-9f510cf0d497
 md"""
 # Fourier Series
-## Orthogonality from circle symmetry
+
+## Elementary Rotations 
 
 We will define the k-th elementary rotation
 
 $E_k(t) = e^{ikt}$
 
 with $k \in \mathbb{Z}$. For simplicity we are keeping the fundamental period $T=2\pi$, but a more general k-th elementary rotation for an arbitrary period $T$ can be defined by setting $\omega_0 = 2\pi/T$ and then $E_k(t) = e^{ik\omega_0 t}$.
+
+## Properties 
 
 Product of two elements add indices:
 
@@ -50,14 +53,21 @@ The complex conjugate of the element is an element with the opposite sign:
 
 $\overline{E_k(t)} = e^{-ikt} = E_{-k}(t)$
 
+## Scalar Product
+
 We define the **scalar product** of two elements as the average over a full period after conjugating the second element
 
 $E_k \cdot E_k = \langle E_k(t) \overline{E_k(t)}\rangle_T = \langle e^{ikt}e^{-ikt} \rangle_T = \langle 1 \rangle_T = 1$ 
 
 """
 
+# ╔═╡ 74ac1574-8b21-499c-891d-70c6e510cfa0
+md"""
+## Elementary Rotations from k=-6 to k=6
+"""
+
 # ╔═╡ 263affc0-a928-4d6f-97e9-48aa6126d1f3
-@bind t_1 Clock(0.1,true,false,401,false)
+@bind t_1 Clock(0.2,true,false,401,false)
 
 # ╔═╡ a2f57914-6a31-4e3b-ab6d-2dadfc76938d
 begin
@@ -69,9 +79,13 @@ end
 
 # ╔═╡ 8338e09a-6751-4aaf-b5a1-8c651e6c5cb8
 md"""
+## Averages 
+
 The average of the elementary rotation over a full period is zero except for $k=0$
 
 $\langle E_k(t) \rangle_T = 0 \quad for \quad k\neq 0$
+
+## Orthogonality 
 
 The scalar product of two elementary rotations:
 
@@ -79,6 +93,9 @@ $E_k \cdot E_j = \langle E_k(t)\overline{E_j(t)} \rangle_T = \langle E_{k-j}(t) 
 
 Therefore, all elementary rotations are orthogonal. Only equal indices survive.
 """
+
+# ╔═╡ 2924bbc2-e3d5-4a80-a8e0-f44f7e7fb6aa
+@bind t_2 Clock(0.1,true,false,401,false)
 
 # ╔═╡ 706bd63b-25d8-4fe8-94cf-217e0f60cc09
 md"""
@@ -100,9 +117,6 @@ $c_k :=  \langle x(t)\overline{E_k(t)} \rangle_T$
 
 """
 
-# ╔═╡ 0ee405e0-f1ae-4648-a061-cd1aac0c0013
-
-
 # ╔═╡ b708f59c-905d-45d8-8a48-70b3bb534af5
 begin
 	stylefile = joinpath(@projectroot,"Pluto","light_33.css")
@@ -113,10 +127,14 @@ end
 html"""
 <style>
 main {
-    max-width: 1000px;
+    max-width: 800px !important;
+	margin-right: 50px !important;
 }
 input[type*="range"] {
 	width: 25%;
+}
+pluto-helpbox {
+    display: none;
 }
 </style>
 """
@@ -132,19 +150,21 @@ j $(@bind j Slider(-6:1:6,default=3;show_value=true))
 
 # ╔═╡ 4f029a92-bcb9-4b11-908c-68688f9d4a6a
 begin
-	p1,p2,p3 = plot_fasor_product(t1,1,1,k,j,0,0,1.2)
+	t2 = mod(t_2-0.99,400)*(4*pi)/400
+	p1,p2,p3 = plot_fasor_product(t2,1,1,k,j,0,0,1.2)
 	plot(p1,p2,p3,layout=(1,3),size=(600,200),left_margin=[-10mm -10mm])
 end	
 
 # ╔═╡ Cell order:
 # ╟─83f8450d-3225-4f37-ba5d-9f510cf0d497
+# ╟─74ac1574-8b21-499c-891d-70c6e510cfa0
 # ╟─263affc0-a928-4d6f-97e9-48aa6126d1f3
 # ╟─a2f57914-6a31-4e3b-ab6d-2dadfc76938d
 # ╟─8338e09a-6751-4aaf-b5a1-8c651e6c5cb8
+# ╟─2924bbc2-e3d5-4a80-a8e0-f44f7e7fb6aa
 # ╟─4f029a92-bcb9-4b11-908c-68688f9d4a6a
 # ╟─fd653582-1eef-4069-a102-315badc98a70
 # ╟─706bd63b-25d8-4fe8-94cf-217e0f60cc09
-# ╠═0ee405e0-f1ae-4648-a061-cd1aac0c0013
 # ╟─45d2b2d7-3e53-44c0-a7b9-56c1794ebc2e
 # ╟─1f093de0-9501-11ef-30d2-4f854ecfb2e5
 # ╟─b708f59c-905d-45d8-8a48-70b3bb534af5
